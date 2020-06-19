4af0f14d8239122ea1079f5905a72444fd1d4891
qmxwby <2621955028@qq.com>
2020-06-19 18:26:04 +0800

    表单页面

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@
 入口JS
  */
 import Vue from 'vue'
-import {Button} from 'mint-ui'
+//import {Button} from 'mint-ui'
 import VueLazyload from 'vue-lazyload'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MINT from 'mint-ui'
+Vue.use(MINT)
 
 import './mock/mockServer' // 加载mockServer即可
 import loading from './common/imgs/loading.gif'
 import './fiters' // 加载过滤器
 
 // 注册全局组件标签
-Vue.component(Button.name, Button)  // <mt-button>
+// Vue.component(Button.name, Button)  // <mt-button>
 Vue.use(VueLazyload, { // 内部自定义一个指令lazy
   loading
 })
